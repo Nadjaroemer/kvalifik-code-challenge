c71ffddeef5ed5f27aba0580d5e9f41c480c83d5
Nadjaroemer <nadja.kuti@gmail.com>
2022-07-24 13:07:02 +0200

    add calendar headers

--- src/components/Calendar.tsx
+++ src/components/Calendar.tsx
@@ -1,26 +1,30 @@
-import { Box } from '@chakra-ui/react'
-import type { FC } from 'react'
+import { Box, Grid } from "@chakra-ui/react";
+import type { FC } from "react";
+
+const weekdays = ["Mo", "Tu", "We", "Th", "Fr", "Sa", "Su"];
 
 type Props = {
-  param?: string
-}
+  param?: string;
+};
 
 const Calendar: FC<Props> = () => {
   return (
-    <Box 
-      w='500px'
-      bg='white'
-      h='400px'
-      borderRadius='13px'
-      boxShadow='0 0 99px 0px rgba(0, 0, 0, 0.04)'
-      mb='10'
-      display='flex'
-      justifyContent='center'
-      alignItems='center'
+    <Box
+      w="500px"
+      bg="white"
+      h="400px"
+      borderRadius="13px"
+      boxShadow="0 0 99px 0px rgba(0, 0, 0, 0.04)"
+      mb="10"
     >
-      <p>Calendar</p>
+      <Box>May 2022</Box>
+      <Box display="grid" gridTemplateColumns="repeat(7, 1fr)">
+        {weekdays.map((weekday) => {
+          return <Box>{weekday}</Box>;
+        })}
+      </Box>
     </Box>
-  )
-}
+  );
+};
 
-export default Calendar
+export default Calendar;
